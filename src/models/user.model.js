9f00dcc8aef53f48939accae2566e3f44acfc1d0
Shipra Goswami <goswamishipra246@gmail.com>
2024-06-05 00:26:26 +0530

    register user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -24,7 +24,7 @@ const userSchema = new Schema(
             trim: true,
             index: true
         },
-        avtar: {
+        avatar: {
             type: String,
             required: true,
         },
