d75cbf2769d3dfb61c7431a882632dee53eca49d
Shipra Goswami <goswamishipra246@gmail.com>
2024-06-08 19:41:09 +0530

    update avatar & cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express"
-import { loginUser, 
-    registerUser, 
-    logoutUser, 
-    refreshAccessToken, 
-    changeCurrentPassword, 
-    getCurrentUser, 
-    updateAccountDetails } from "../controllers/user.controller.js"
+import {
+    loginUser,
+    registerUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.js"
 import { verifyJWT } from "../middlewares/auth.js"
 
@@ -33,5 +37,8 @@ router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 export default router
